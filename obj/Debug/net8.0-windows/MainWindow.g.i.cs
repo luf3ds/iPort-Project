61e25c61a700b0718af91ffb6b38e605ef794698
lufe <lufeds@outlook.com>
2026-05-22 00:00:09 -0300

    JSON add

--- obj/Debug/net8.0-windows/MainWindow.g.i.cs
+++ obj/Debug/net8.0-windows/MainWindow.g.i.cs
@@ -167,7 +167,7 @@ namespace WpfHackthon {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WpfHackthon;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WpfHackthon;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
